644f960407f9dc6eb74d6891192453661ca5374d
SaltyChickenLeg <merijnjong1@gmail.com>
2024-04-24 17:06:31 +0200

    started learning about fork function, pipe function and running multiple processes at the same time

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:19:17 by mjong             #+#    #+#             */
-/*   Updated: 2024/04/23 18:20:27 by mjong            ###   ########.fr       */
+/*   Updated: 2024/04/24 14:56:34 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@
 # include <stdio.h>
 # include <limits.h>
 # include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <errno.h>
 
 #endif
