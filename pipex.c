2797494f5f71379073b3d1eb185df22f4562c92d
SaltyChickenLeg <merijnjong1@gmail.com>
2024-05-23 18:15:09 +0200

    finished the project

--- pipex.c
+++ pipex.c
@@ -1,47 +1,63 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   pipex_backup.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:18:50 by mjong             #+#    #+#             */
-/*   Updated: 2024/05/16 16:12:11 by mjong            ###   ########.fr       */
+/*   Updated: 2024/05/23 18:13:18 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	ft_child1_process(char *argv[], char *envp[], int *fd)
+pid_t	ft_child1_process(char *argv[], char *envp[], int *fd)
 {
-	int	infile;
+	int		infile;
+	pid_t	pid;
 
-	close(fd[0]);
-	infile = open(argv[1], O_RDONLY, 0777);
-	if (infile == -1)
-		ft_error("infile not found");
-	dup2(infile, 0);
-	close(infile);
-	dup2(fd[1], 1);
-	close(fd[1]);
-	ft_execute(argv[2], envp);
+	pid = fork();
+	if (pid == -1)
+		ft_error("fork");
+	if (pid == 0)
+	{
+		close(fd[0]);
+		infile = open(argv[1], O_RDONLY, 0777);
+		if (infile == -1)
+			ft_error("infile not found");
+		dup2(infile, 0);
+		close(infile);
+		dup2(fd[1], 1);
+		close(fd[1]);
+		ft_execute(argv[2], envp);
+	}
+	return (pid);
 }
 
-void	ft_child2_process(char *argv[], char *envp[], int *fd)
+pid_t	ft_child2_process(char *argv[], char *envp[], int *fd)
 {
-	int	outfile;
+	int		outfile;
+	pid_t	pid;
 
-	close(fd[1]);
-	outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	if (outfile == -1)
-		ft_error("outfile not found");
-	dup2(outfile, 1);
-	close(outfile);
-	dup2(fd[0], 0);
-	close(fd[0]);
-	if (ft_strcmp(argv[3], "") == 0)
-		ft_error("argv 3");
-	ft_execute(argv[3], envp);
+	pid = fork();
+	if (pid == -1)
+		ft_error("fork");
+	if (pid == 0)
+	{
+		close(fd[1]);
+		outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		if (outfile == -1)
+			ft_error("outfile not found");
+		dup2(outfile, 1);
+		close(outfile);
+		dup2(fd[0], 0);
+		close(fd[0]);
+		if (ft_strcmp(argv[3], "") == 0)
+			ft_error("argv 3");
+		ft_execute(argv[3], envp);
+	}
+	return (pid);
 }
 
 int	main(int argc, char *argv[], char *envp[])
@@ -50,34 +66,19 @@ int	main(int argc, char *argv[], char *envp[])
 	int		fd[2];
 	int		status;
 
-	if (argc == 5)
-	{
-		if (pipe(fd) == -1)
-			ft_error("pipe");
-		pid = fork();
-		if (pid == -1)
-			ft_error("fork");
-		if (pid == 0)
-		{
-			ft_child1_process(argv, envp, fd);
-			exit(1);
-		}
-		close(fd[1]);
-		wait(NULL);
-		pid = fork();
-		if (pid == -1)
-			ft_error("fork");
-		if (pid == 0)
-		{
-			ft_child2_process(argv, envp, fd);
-			exit(1);
-		}
-		waitpid(pid, &status, 0);
-	}
-	else
-	{
-		ft_putstr_fd("\033[31mError: Incorrect notation\n\e[0m", 2);
-		ft_putstr_fd("Example: ./pipex <file1> <cmd1> <cmd2> <file2>\n", 1);
-	}
-	return (0);
+	if (argc != 5)
+		ft_error("\033[31mError: Incorrect notation\n\e[0m\n\
+		Example: ./pipex <file1> <cmd1> <cmd2> <file2>\n");
+	if (pipe(fd) == -1)
+		ft_error("pipe");
+	pid = ft_child1_process(argv, envp, fd);
+	pid = ft_child2_process(argv, envp, fd);
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid, &status, 0);
+	while (wait(NULL) != -1)
+		;
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	return (EXIT_FAILURE);
 }
