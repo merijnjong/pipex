50a2e0177006fd530e5fe9c669e1686f3925bfda
SaltyChickenLeg <merijnjong1@gmail.com>
2024-06-11 13:42:09 +0200

    finished

--- pipex_utils.c
+++ pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 13:57:07 by mjong             #+#    #+#             */
-/*   Updated: 2024/05/23 18:08:27 by mjong            ###   ########.fr       */
+/*   Updated: 2024/06/11 13:39:26 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ char	*ft_find_path(char *envp[], char *cmd)
 	int		i;
 
 	i = 0;
+	if (access(cmd, F_OK) == 0)
+			return (cmd);
 	while (ft_strnstr(envp[i], "PATH", 4) == 0)
 		i++;
 	paths = ft_split(envp[i] + 5, ':');
